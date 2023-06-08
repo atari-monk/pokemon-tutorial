e0aa796bcf9a953127581f46e4a5a60d5d9eb4f8
atari-monk <atari.monk1@gmail.com>
2023-06-08 22:37:08 +0200

    prod path

--- src/js/data/audio.js
+++ src/js/data/audio.js
@@ -1,38 +1,42 @@
 const defaultVolume = 0.1;
 const highVolume = 0.5;
+const prod = 'https://atari-monk.github.io/pokemon-tutorial/assets/audio/';
+const local = './../../assets/audio/';
+const isProd = true;
+const host = isProd ? prod : local;
 export const audio = {
   map: new Howl({
-    src: './../../assets/audio/map.wav',
+    src: host + 'map.wav',
     html5: true,
     volume: defaultVolume,
   }),
   initBattle: new Howl({
-    src: './../../assets/audio/initBattle.wav',
+    src: host + 'initBattle.wav',
     html5: true,
     volume: defaultVolume,
   }),
   battle: new Howl({
-    src: './../../assets/audio/battle.mp3',
+    src: host + 'battle.mp3',
     html5: true,
     volume: defaultVolume,
   }),
   tackleHit: new Howl({
-    src: './../../assets/audio/tackleHit.wav',
+    src: host + 'tackleHit.wav',
     html5: true,
     volume: defaultVolume,
   }),
   fireballHit: new Howl({
-    src: './../../assets/audio/fireballHit.wav',
+    src: host + 'fireballHit.wav',
     html5: true,
     volume: defaultVolume,
   }),
   initFireball: new Howl({
-    src: './../../assets/audio/initFireball.wav',
+    src: host + 'initFireball.wav',
     html5: true,
     volume: defaultVolume,
   }),
   victory: new Howl({
-    src: './../../assets/audio/victory.wav',
+    src: host + 'victory.wav',
     html5: true,
     volume: highVolume,
   }),
